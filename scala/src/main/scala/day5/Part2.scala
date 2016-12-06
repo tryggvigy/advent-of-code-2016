1ac5af8ca52b5da923a24d1a80f40555433693dd
Tryggvi Gylfason <tryggvigy@gmail.com>
2016-12-06 05:26:56 +0000

    Remove logs from d5p2

--- scala/src/main/scala/day5/Part2.scala
+++ scala/src/main/scala/day5/Part2.scala
@@ -18,25 +18,21 @@ object Part2 {
     }
 
   def findPassCode(doorId: String): String = {
-    var passcode = "GGGGGGGG" // G in invalid in hex. Initialize as 8 G's
+    var passcode = "________"
     var count = 0
 
-    while (passcode.contains("G")) {
+    while (passcode.contains("_")) {
       val hash = md5(doorId + count)
       val posChar = hash.drop(5).take(1)
       if (hash.startsWith("00000") && posCharIsValid(posChar)) {
         val code = hash.drop(6).take(1)
-        if (passcode.charAt(posChar.toInt) == 'G') {
-          println("candidate found")
-          println("hash: ", hash)
-          println("count: ", count)
+        if (passcode.charAt(posChar.toInt) == '_') {
           passcode = passcode.split("").updated(posChar.toInt, code).mkString
+          println(passcode)
         }
       }
       count = count + 1
     }
-    println("DONE!")
-    println("count is: ", count)
     passcode
   }
 
